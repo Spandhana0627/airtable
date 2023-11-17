49552e88b93202a2a01624a2542a3347d96db61f
spandhana0627 <spandhunarne2721@gmail.com>
2023-11-17 11:43:30 +0530

    coding standards update

--- src/Controller/CastsController.php
+++ src/Controller/CastsController.php
@@ -1,61 +1,43 @@
 <?php
-
 namespace Drupal\video_library\Controller;
-
+ 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Drupal\Component\Serialization\Json;
 use Drupal\node\Entity\Node;
-// use Drupal\node\NodeInterface;
-
-class CastsController extends ControllerBase {
-    public function getDatas() {
+ 
+class CastsController extends ControllerBase
+{
+    public function getDatas()
+    {
         $client =\Drupal::httpClient();
         $headers = ['Authorization' => 'Bearer patEop5Jbx2u8t6vb.0f55cfd2cbafc09b90dc73ce78b38aded9603dd00ae068beee620a6577bb7989'];
         //$headers = ['Content-Type' => 'application/json'];
         $request = $client->get(
-           "https://api.airtable.com/v0/appYs8zO2euY9axdz/tblcTxbKQwaLBLjUU",['headers'=> $headers]
+            "https://api.airtable.com/v0/appYs8zO2euY9axdz/tblcTxbKQwaLBLjUU", ['headers'=> $headers]
         );
         $response = json_decode($request->getBody());
-
+ 
         if (!empty($response->records)) {
             foreach ($response->records as $record) {
                 $firstName = $record->fields->First_Name;
                 $lastName = $record->fields->Last_Name;
-                $node = Node::create([
-                    'type' => 'casts', // Replace with your content type machine name.
-
-                    'title' => $firstName .''. $lastName,
+                $node = Node::create(
+                    [
+                    'type' => 'casts',
+                    'title' => $firstName . ''. $lastName,
                     'field_id1' => $record->fields->id ?? '',
-                    // 'field_last' => $record->fields->LastName ?? '',
                     'field_first_name' => $firstName,
                     'field_last_name' => $lastName,
                     'field_description' => $record->fields->description ?? '',
-
-                ]);
+                    ]
+                );
                 $node->save();
             }
-
+ 
         }
         return new JsonResponse(['message' => 'Data imported from Airtable']);
-        // return new JsonResponse($response);
-//         $result=[];
+    }    
 
-//         $query = \Drupal::entityQuery('node')
-//   ->condition('type', 'page')
-//   ->condition('field_cast1', 'olaf', '=')
-//   ->accessCheck(TRUE);
-// $results = $query->execute();
-//         // $query = \Drupal::entityQuery('node')
-//         //               ->condition('type', 'videos');
-//         //             $nodes_ids = $query->execute();
-//         //             if ($nodes_ids) {
-//         //                 foreach ($nodes_ids as $node_id) {
-//         //                     $node = \Drupal\node\Entity\Node::load($node_id);
-//         //                     $values = $node->get('field_casts1')->value;
-//         //                     kint($values);
-//         //                 }
-//         //             }
-//                     return $results;
-    }
+ 
 }
--- src/Controller/CustomAirtableController.php
+++ src/Controller/CustomAirtableController.php
@@ -1,7 +1,7 @@
 <?php
-
+ 
 namespace Drupal\video_library\Controller;
-
+ 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Drupal\Component\Serialization\Json;
@@ -9,97 +9,72 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use GuzzleHttp\ClientInterface;
 use Drupal\node\Entity\Node;
 use Drupal\Core\DateTime\DrupalDateTime;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-
+use Drupal\core\Entity\EntityTypeManagerInterface;
  
-
-class CustomAirtableController extends ControllerBase {
-
+ 
+ 
+class CustomAirtableController extends ControllerBase
+{
+ 
     protected $httpClient;
-    public function __construct(ClientInterface $http_client) {
+    public function __construct(ClientInterface $http_client)
+    {
         $this->httpClient = $http_client;
     }
  
-
-  public static function create(ContainerInterface $container) {
-
-    return new static(
-
-      $container->get('http_client')
-
-    );
-
-  }
-
-  public function fetchDataFromAirtable() {
-
-    $api_url = 'https://api.airtable.com/v0/appYs8zO2euY9axdz/tbltm4cQAW0ZQ0jA9';
-    $headers = [
-
-      'Authorization' => 'Bearer patti0WJTJtiQnIWC.b8d1ee3da7ab5eda08e807c0a9dce15271bfef621d19451a3a84c1a3688ce4ce',
-
-    ];
-    $response = $this->httpClient->request('GET', $api_url, [
-
-      'headers' => $headers,
-
-    ]);
-    $data = json_decode($response->getBody());
-
-    $nids = \Drupal::entityQuery('node')->condition('type','casts')->execute();
-    $nodes = Node::loadMultiple($nids);
-    foreach($nodes as $node){
-      $first_name[] = $node->get('field_first_name')->value;
-    }
-
  
-
-    if (!empty($data->records)) {
-
-      foreach ($data->records as $record) {
-        $record_first_name = $record->fields->Casts;
-        if(in_array($record_first_name,$first_names)){
-          $id = \Drupal::entityQuery('node')->condition('type','casts')->condition('field_first_name',$record_first_name)->execute();
-
-        }
-     
-
-        $node = Node::create([
-
-          'type' => 'videos', // Replace with your content type machine name.
-
-          'title' => $record->fields->Name ?? '',
-          'field_id' => $record->id ?? '',
-          'field_language' => $record->fields->Language ?? '',
-          'field_genre' => $record->fields->Genre ?? '',
-          'field_casts' => $id ?? '',
-          'field_length' => $record->fields->Length ?? '',
-        //   'field_createdtime' =>$test_time,
-
-        
-
-          // Add more field mappings here.
-
-        ]);
-        $node->save();
-
-      }
-
+    public static function create(ContainerInterface $container)
+    {
+ 
+        return new static(
+            $container->get('http_client')
+        );
+ 
     }
-    return new JsonResponse(['message' => 'Data imported from Airtable']);
-
-  }
-
-  /**
-   * 
-   */
-//  private function getCastsId ($Casts_name) {
-//   $connection = Database::Connection();
-//   $query = EntityQuery();
-//   $this->$EntityQuery();
-//   $EntityQuery("SELECT * FROM Casts WHERE FirstName LIKE olaf"); //Casts table like query 
-  
-
-//  }
-
+ 
+    public function fetchDataFromAirtable()
+    {
+ 
+        $api_url = 'https://api.airtable.com/v0/appYs8zO2euY9axdz/tbltm4cQAW0ZQ0jA9';
+        $headers = [
+        'Authorization' => 'Bearer
+        patti0WJTJtiQnIWC.b8d1ee3da7ab5eda08e807c0a9dce15271bfef621d19451a3a84c1a3688ce4ce',
+        ];
+        $response = $this->httpClient->request(
+            'GET', $api_url, [
+            'headers' => $headers,
+            ]
+        );
+        $data = json_decode($response->getBody());
+ 
+        $nids = \Drupal::entityQuery('node')->condition('type', 'casts')->execute();
+        $nodes= Node::loadMultiple($nids);
+        foreach($nodes as $node){
+            $first_names[] = $node->get('field_first_name')->value;
+        }
+        if (!empty($data->records)) {
+            foreach ($data->records as $record) {
+                $record_first_name = $record->fields->Casts;
+                if(in_array($record_first_name, $first_names)) {
+                    $id = \Drupal::entityQuery('node')->condition('type', 'casts');
+                    $id = $id->condition('field_first_name', $record_first_name)->execute();
+                }
+           
+                $node = Node::create(
+                    [
+                    'type' => 'videos', // Replace with your content type machine name.
+                    'title' => $record->fields->Name ?? '',
+                    'field_id' => $record->id ?? '',
+                    'field_language' => $record->fields->Language ?? '',
+                    'field_genre' => $record->fields->Genre ?? '',
+                    'field_length' => $record->fields->Length ?? '',
+                    'field_casts' => $id ?? '',
+                    ]
+                );
+                $node->save();
+            }
+        }
+        return new JsonResponse(['message' => 'Data imported from Airtable']);
+    }
+ 
 }
